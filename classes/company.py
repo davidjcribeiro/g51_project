e380b9cf1ed028af001190678b58b898e1b88fe7
ClaudioFeup <up202505073@up.pt>
2026-04-26 00:49:54 +0100

    classes2

--- classes/company.py
+++ classes/company.py
@@ -1,2 +1,32 @@
-# -*- coding: utf-8 -*-
+import Gclass
+class Company(Gclass):
+    obj = {}
+    lst = []
+    pos = -1
+    sortkey = ''
+    att = ['_id', '_name', '_creation_date']
+    headers = 'Empresas de Energia'
+    des = ['ID Empresa', 'Nome', 'Data de Criação']
 
+    def __init__(self, id=0, name="", creation_date=""):
+        super().__init__()
+        self._id = Company.get_id(id)
+        self._name = name
+        self._creation_date = creation_date
+       
+        # Registo obrigatório
+        Company.obj[self._id] = self
+        Company.lst.append(self._id)
+
+    @property
+    def id(self): return self._id
+   
+    @property
+    def name(self): return self._name
+    @name.setter
+    def name(self, value): self._name = value
+
+    @property
+    def creation_date(self): return self._creation_date
+    @creation_date.setter
+    def creation_date(self, value): self._creation_date = value
--- classes/plant.py
+++ classes/plant.py
@@ -1,2 +1,33 @@
-# -*- coding: utf-8 -*-
+import Gclass
+class Plant(Gclass):
+    obj = {}
+    lst = []
+    pos = -1
+    sortkey = ''
+    att = ['_id', '_comments', '_company_id']
+    headers = 'Centrais de Produção'
+    des = ['ID Central', 'Comentários', 'ID Empresa Proprietária']
+
+    def __init__(self, id=0, comments="", company_id=0):
+        super().__init__()
+        self._id = Plant.get_id(id)
+        self._comments = comments
+        self._company_id = company_id
+       
+        Plant.obj[self._id] = self
+        Plant.lst.append(self._id)
+
+    @property
+    def id(self): return self._id
+
+    @property
+    def comments(self): return self._comments
+    @comments.setter
+    def comments(self, value): self._comments = value
+
+    @property
+    def company_id(self): return self._company_id
+    @company_id.setter
+    def company_id(self, value): self._company_id = value
+
 
